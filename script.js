8b3376650bf84c304b84fef12a1544ec9595c8af
ajaysingh40cr-maker <ajaysingh40cr@gmail.com>
2026-07-16 13:28:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,37 +1,23 @@
-document.getElementById("loginBtn").onclick = function(){
+function login() {
 
-let user = document.getElementById("userid").value;
-let pass = document.getElementById("password").value;
+let user = document.getElementById("userid").value.trim();
+let pass = document.getElementById("password").value.trim();
 let msg = document.getElementById("msg");
 
 if(user==="AJAY@MU2026" && pass==="993428"){
 
-msg.style.color="lime";
-msg.innerHTML="Login Successful...";
+msg.style.color="#00ff88";
+msg.innerHTML="✅ Login Successful...";
 
 setTimeout(function(){
-
 window.location.href="dashboard.html";
-
 },1000);
 
 }else{
 
-msg.style.color="red";
-msg.innerHTML="Invalid User ID or Password";
+msg.style.color="#ff4d4d";
+msg.innerHTML="❌ Invalid User ID or Password";
 
 }
 
-};
-
-document.getElementById("adminBtn").onclick=function(){
-
-alert(
-"Customer Support\n\n"+
-"Primary Number: +91 9572025885\n"+
-"Secondary Number: +91 8294497070\n"+
-"Email: ajaysingh40cr@gmail.com\n\n"+
-"Company Address:\nGlobal Pay Support Office\nOffice 1204, Bay Square,\nBusiness Bay,\nDubai, United Arab Emirates"
-);
-
-};
+}
