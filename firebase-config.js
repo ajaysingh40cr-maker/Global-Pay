9925469b5e3cdf0548270813ee62aa9f096df7ad
globalpaypanel <ajaysingh40cr@gmail.com>
2026-07-18 12:33:29 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,13 @@
 // Firebase Config
 const firebaseConfig = {
-  apiKey: "YAHAN_APNA_API_KEY",
+  apiKey: "AIzaSyCL8vjtmJtSIJyax22qaRb3as2UPvmcVEE",
   authDomain: "global-pay-panel.firebaseapp.com",
+  databaseURL: "https://global-pay-panel-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "global-pay-panel",
   storageBucket: "global-pay-panel.firebasestorage.app",
   messagingSenderId: "50490595463",
-  appId: "1:50490595463:web:69457a3db6634522b3c022"
+  appId: "1:50490595463:web:69457a3db6634522b3c022",
+  measurementId: "G-ENPBE841ZT"
 };
 
 // Initialize Firebase
